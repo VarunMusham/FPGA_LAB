493157a35901b51ee0c891e0aff7d6bcab3e7a19
Musham Varun <mushamvarun130@gmail.com>
2026-03-15 21:47:50 +0530

    Temperature Sensor Project Update

---
+++ Temp_Sensor/Temp_Sensor.srcs/sim_1/new/i2c_master_TB.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+// Test bench for i2c_master.v
+// Concerning the Temp Sensor on Nexys A7
+// Created by David Marion
+// Date 7.19.2022
+
+module i2c_master_TB;
+	reg clk;
+	reg reset;
+	wire SDA;
+	wire SDA_dir;
+	wire SCL;
+	
+	i2c_master master(
+		.clk_200kHz(clk),
+		.reset(reset),
+		.SDA(SDA),
+		.SDA_dir(SDA_dir),
+		.SCL(SCL)
+	);
+
+	always #1 clk = !clk;
+	
+	
+	initial begin
+		clk = 1;
+		reset = 0;
+		
+		#30000 $finish;
+	end
+
+endmodule
